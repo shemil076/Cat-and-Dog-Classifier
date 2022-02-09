0ee61c73e4996377d726ea1b73d6223ed22a28c2
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-09 14:21:12 +0530

    Implemented the app succesfully

--- lib/home.dart
+++ lib/home.dart
@@ -12,23 +12,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  final  picker = ImagePicker();
-    late File _image;
-    bool _loading = false;
-     late List _output;
+  final picker = ImagePicker();
+  late File _image;
+  bool _loading = false;
+  late List _output;
 
   pickImage() async {
     var image = await picker.getImage(source: ImageSource.camera);
 
-     if (image == null) return null;
+    if (image == null) return null;
 
-     setState(() {
-       _image = File(image.path);
-     });
+    setState(() {
+      _image = File(image.path);
+    });
     classifyImage(_image);
   }
-
 
   pickGalleryImage() async {
     var image = await picker.getImage(source: ImageSource.gallery);
@@ -42,13 +40,10 @@ class _HomeState extends State<Home> {
     classifyImage(_image);
   }
 
-
   void initState() {
     super.initState();
     _loading = true;
-    loadModel().then((value){
-
-    });
+    loadModel().then((value) {});
   }
 
   void dispose() {
@@ -57,19 +52,23 @@ class _HomeState extends State<Home> {
   }
 
   loadModel() async {
-    await Tflite.loadModel(model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
   }
 
   classifyImage(File image) async {
-    var output = await Tflite.runModelOnImage(path: image.path, numResults: 2, threshold: 0.5, imageMean: 127.5, imageStd: 127.5);
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
 
-
-    setState((){
+    setState(() {
       _loading = false;
       _output = output!;
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -89,55 +88,78 @@ class _HomeState extends State<Home> {
                 ),
               ),
               SizedBox(height: 6),
-              Text('Detect Dogs and Cats', style: TextStyle(color: Color(0xFFE99600),
-              fontWeight: FontWeight.w500, fontSize:28
-              )),
+              Text('Detect Dogs and Cats',
+                  style: TextStyle(
+                      color: Color(0xFFE99600),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 28)),
               SizedBox(height: 50),
               Center(
-                child: Container(
-                  width: 300,
-                    child: Column(
-                      children: [
-                        Image.asset('assets/cat.png'),
-                        SizedBox(height: 50),
-                      ],
-                    ),
-                ),
+                child: _loading
+                    ? Container(
+                        width: 300,
+                        child: Column(
+                          children: [
+                            Image.asset('assets/cat.png'),
+                            SizedBox(height: 50),
+                          ],
+                        ),
+                      )
+                    : Container(
+                        child: Column(
+                        children: [
+                          Container(
+                            height: 250,
+                            child: Image.file(_image),
+                          ),
+                          SizedBox(height: 20),
+
+                          _output != null ?
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                              child: Text('${_output[0]['label']}', style: TextStyle(color: Colors.white, fontSize: 20.0))):
+                              Container(),
+                        ],
+                      )),
               ),
               Container(
-                width : MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    GestureDetector(onTap: pickImage,
-                    child: Container(
-                      width : MediaQuery.of(context).size.width -260,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 17),
-                      decoration:  BoxDecoration(
-                        color: Color(0xFFE99600),
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                        child: Text('Take a photo',
-                        style: TextStyle(color: Colors.white,)),
-                    ),
-                    ),
-                    SizedBox(
-                      width:20
-                    ),
-                    GestureDetector(onTap: pickGalleryImage,
+                    GestureDetector(
+                      onTap: pickImage,
                       child: Container(
-                        width : MediaQuery.of(context).size.width -260,
+                        width: MediaQuery.of(context).size.width - 260,
                         alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 24, vertical: 17),
-                        decoration:  BoxDecoration(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFE99600),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Text('Take a photo',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: pickGalleryImage,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 260,
+                        alignment: Alignment.center,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                        decoration: BoxDecoration(
                           color: Color(0xFFE99600),
                           borderRadius: BorderRadius.circular(6),
                         ),
                         child: Text('Camera Roll',
-                            style: TextStyle(color: Colors.white,)),
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
                       ),
                     ),
                   ],
